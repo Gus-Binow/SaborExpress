b9da518ff646724ae775b5d7a1b58cd8f7f8d196
Gustavo Binow <gustavobinow2001@hotmail.com>
2025-10-15 20:53:51 -0300

    Códigos

---
+++ Exercícios/Número 02.py
@@ -0,0 +1,6 @@
+#Imprima a frase: Meu nome é {nome} e tenho {idade} anos
+#em que nome e idade precisam ser valores armazenados em variáveis.
+meu_nome = ('Gustavo')
+minha_idade = 24
+
+print(f'Meu nome é {meu_nome} e tenho {minha_idade} anos')
---
+++ app.py
@@ -0,0 +1,30 @@
+print('''
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+''')
+
+#menu de escolha inicial
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+#escolha do menu inicial
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcao_escolhida}')
+
+if opcao_escolhida == 1:
+    print('Cadastrar Restaurante')
+
+elif opcao_escolhida == 2:
+    print('Listar Restaurante')
+
+elif opcao_escolhida == 3:
+    print('Ativar Restaurante:')
+
+else:
+    print('Encerrando o programa')
