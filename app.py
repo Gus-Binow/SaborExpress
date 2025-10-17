6ad9373ee4bd60cbd4b854930165b1b4d5cbc0e1
Gustavo Binow <gustavobinow2001@hotmail.com>
2025-10-17 17:46:55 -0300

    V.05 - Melhoria de quebra

--- app.py
+++ app.py
@@ -23,19 +23,30 @@ def finalizar_app():
     #os.system('clear') no mac
     print('Finalizando o programa\n')
 
+def opcao_invalida():
+    print('Opção invalida!')
+    input('Digite uma tecla para voltar ao menu principal  ')
+    main()
+
 #escolha uma opção
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: '))
-    #opcao_escolhida = int(opcao_escolhida) || outra opção em relação a linha de cima
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: '))
+        #opcao_escolhida = int(opcao_escolhida) || outra opção em relação a linha de cima
 
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurante')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante:')
-    else:
-        finalizar_app()
+        if opcao_escolhida == 1:
+            print('Cadastrar restaurante')
+        elif opcao_escolhida == 2:
+            print('Listar restaurante')
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+
+    except:
+        opcao_invalida()
 
 
 #alternativo de código no lugar de il elif else
@@ -55,6 +66,7 @@ match opcao_escolhida:
 '''
 
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
