65f94e8f6af917f22369b8359e39be247cf0c485
Gustavo Binow <gustavobinow2001@hotmail.com>
2025-10-17 06:53:17 -0300

    Exercício de prática 08
    Sistema de coordenadas

---
+++ Exercícios/Número 08.py
@@ -0,0 +1,27 @@
+'''
+Solicite ao usuário as coordenadas (x, y) de um ponto qualquer e utilize uma estrutura if elif else para determinar 
+em qual quadrante do plano cartesiano o ponto se encontra de acordo com as seguintes condições:
+
+Primeiro Quadrante: os valores de x e y devem ser maiores que zero;
+Segundo Quadrante: o valor de x é menor que zero e o valor de y é maior que zero;
+Terceiro Quadrante: os valores de x e y devem ser menores que zero;
+Quarto Quadrante: o valor de x é maior que zero e o valor de y é menor que zero;
+Caso contrário: o ponto está localizado no eixo ou origem.
+'''
+import os
+os.system('cls')
+
+#entrada das coordenadas, caso as coordenadas sejam colocadas com "," as mesmas serão substituidas por "."
+coordenada_x = float(input('Digite a coordenada X: ').replace(",", "."))
+coordenada_y = float(input('Digite a coordenada Y: ').replace(",", "."))
+
+#Localização dos quadrantes
+if coordenada_x > 0 and coordenada_y > 0:
+    print('\nAs coordenadas se encontram no primeiro quadrante.')
+elif coordenada_x < 0 and coordenada_y > 0:
+    print('\nAs coordenadas se encontram no segundo quadrante.')
+elif coordenada_x < 0 and coordenada_y < 0:
+    print('\nAs coordenadas se encontram no terceiro quadrante.')
+elif coordenada_x > 0 and coordenada_y < 0:
+    print('\nAs coordenadas se encontram no quarto quadrante.')
+
