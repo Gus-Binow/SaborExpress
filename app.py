0eb28eb0e4a0445e0a0014783ab1856a5ab2c478
Gustavo Binow <gustavobinow2001@hotmail.com>
2025-10-21 17:35:04 -0300

    V.07 - listagem de restaurantes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-restaurantes = []
+restaurantes = ['Pizza', 'Sushi']
 
 
 def exibir_nome_do_programa():
@@ -38,9 +38,20 @@ def cadastrar_novo_restaurante():
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-    input('Digite uma tecla para voltar ao menu principal')
+
+    input('\nDigite uma tecla para voltar ao menu principal')
     main()
-    
+
+def listar_restaurante():
+    os.system('cls')
+    print('Listando os restaurantes:\n')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    input('\nDigite uma tecla para voltar ao menu principal')
+    main()
+
 #escolha uma opção
 def escolher_opcao():
     try:
@@ -50,7 +61,7 @@ def escolher_opcao():
         if opcao_escolhida == 1:
             cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
-            print('Listar restaurante')
+            listar_restaurante()
         elif opcao_escolhida == 3:
             print('Ativar restaurante')
         elif opcao_escolhida == 4:
